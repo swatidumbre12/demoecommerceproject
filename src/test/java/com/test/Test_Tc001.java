1647156ce82074b155af4de392d3d1adc3d7e59f
swatidumbre12 <dswati2903@gmail.com>
2026-05-27 18:50:57 +0530

    swati test12

---
+++ src/test/java/com/test/Test_Tc001.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+public class Test_Tc001 {
+	
+	@Test
+	public static void TestTc001() {
+		
+		
+		System.out.println("TestTc001");
+		
+		}
+
+}
